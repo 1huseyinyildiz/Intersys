60cede08b26d72763430d507876b16012aa88928
HÜSEYİN YILDIZ <33803141+1huseyinyildiz@users.noreply.github.com>
2023-01-26 16:41:15 +0100

    Add files via upload

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string cleanedString = Regex.Replace(input, "[^a-zA-Z]", "").ToLower();
+        bool isPalindrome = cleanedString.SequenceEqual(cleanedString.Reverse());
+        Console.WriteLine(isPalindrome ? "YES" : "NO");
+    }
+}
+ 
